4968f0f37041a6b33a85495050fedd3cca349787
Yixin <yixin88@hotmail.co.uk>
2023-02-04 14:24:35 +0000

    added careers page with the local api, with added styling. Using react Loader

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 //layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
+import CareerLayout from "./layouts/CareerLayout";
 
 
 
@@ -18,9 +20,14 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />}/>
       <Route path="about" element={<About />}/>
+
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />}/>
         <Route path="contact" element={<Contact />}/>
+      </Route>
+
+      <Route path="careers" element={<CareerLayout />}>
+        <Route index element={<Careers />} loader={careersLoader}/>
       </Route>
 
       <Route path="*" element={<NotFound />} />
