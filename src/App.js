0561afbfc41399c8883e02f98497fa5c7026f3d0
Yixin <yixin88@hotmail.co.uk>
2023-02-05 21:13:14 +0000

    added React Form component that also check if msg length is greater than 10 chars

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createBrowserRouter, Route, NavLink, createRoutesFromElements, RouterPr
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
+import Contact, { contactAction } from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
 import Careers, { careersLoader } from "./pages/careers/Careers";
 import CareerDetail, { careerDetailsLoader } from "./pages/careers/CareerDetail";
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
 
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />}/>
-        <Route path="contact" element={<Contact />}/>
+        <Route path="contact" element={<Contact />} action={contactAction}/>
       </Route>
 
       <Route path="careers" element={<CareerLayout />} errorElement={<CareersError />}>
