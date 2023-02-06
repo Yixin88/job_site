0548a8b5ac9ec61c96cdd00da8d036a6d28dba8c
Yixin <yixin88@hotmail.co.uk>
2023-02-06 00:19:17 +0000

    added react navigate to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
+
 export default function About() {
+    const [ user, setUser] = useState('Yixin')
+
+    if (!user) {
+        return <Navigate to='/' replace={true} />
+    }
+
     return (
         <div className="About">
             <p>
-                Abouts pages
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Explicabo, adipisci ipsam illo quo repellendus placeat tempora, 
+                pariatur sint fugiat ducimus molestiae rerum voluptas, 
+                accusamus necessitatibus molestias iure quidem? Quam, laboriosam.
             </p>
+
+            <button onClick={() => setUser(null)}>Logout</button>
         </div>
     )
 }
